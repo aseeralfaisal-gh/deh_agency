6c197f3d739e700c5e7ffe4195576bdd2c4aa850
aseeralfaisal-gh <aseeralfaisal@gmail.com>
2020-09-30 01:27:11 +0600

    311

---
+++ Home-Screen.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>Detective Hiring Agency- Home</title>
+        <link rel="stylesheet" href="dez.css"> 
+        <script src="https://kit.fontawesome.com/c64b631310.js" crossorigin="anonymous"></script>
+    </head>    
+    <body>  
+        <i class="fas fa-eye"></i>
+        <span class="title">Detective Hiring Agency</span>        
+        <div class="nav">
+            <ol>
+                <li><i class="fas fa-cash-register"></i> <a href="registration.php">Register</li>
+                    <li><i class="fas fa-sign-in-alt"></i> <a href="login.php">Login</li>
+                        <li><i class="fas fa-address-card"></i> <a href="about.php">About</a></li>                      
+                    </ol>                  
+        </div>
+        <div class="slide">
+        </div>  
+        <div class="bars">            
+            <h3>Murder</h3>
+            <div class="grey">
+                <div class="murder"></div>
+            </div>
+            <h3>Spy Someone</h3>
+            <div class="grey">
+                <div class="spy"></div>
+            </div>
+            <h3>Kidnapping</h3>
+            <div class="grey">
+                <div class="kidnap"></div>
+            </div>
+            <h3>Lost Something</h3>
+            <div class="grey">
+                <div class="lost"></div>
+            </div>  
+            <h3>High Tech Crime</h3>
+            <div class="grey">
+                <div class="high"></div>  
+            </div>
+            <div class="about">
+                <h3>About Us</h3>
+                <h4>
+                    Dhaka, founded as the Dhaka National Detective Agency, 
+                    is a private security guard and detective agency established in the Bangladesh by Rifat, Aseer, Neha and Ahnaf 
+                    in 2020 and currently a subsidiary of Securitas AB.Dhaka Agency became famous when he claimed to have foiled 
+                    a plot to assassinate Some famous people.
+                    They later hired Dhaka agents for his personal security during the Civil War.
+                </h4>
+            </div>      
+    </body>
+</html>
